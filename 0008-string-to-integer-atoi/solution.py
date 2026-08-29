cfa33d564a1c0ed43f3ac75f0fa38dc30fdd6646
MalahimHaseeb <140875619+MalahimHaseeb@users.noreply.github.com>
2026-08-29 11:35:43 +0500

    Create solution.py

---
+++ 0008-string-to-integer-atoi/solution.py
@@ -0,0 +1,33 @@
+class Solution:
+    def myAtoi(self, s: str) -> int:
+        s = s.strip()
+
+        if not s:
+            return 0
+
+        i = 0
+        sign = 1
+
+       
+        if s[i] == '-':
+            sign = -1
+            i += 1
+        elif s[i] == '+':
+            i += 1
+
+        num = 0
+
+     
+        while i < len(s) and s[i].isdigit():
+            num = num * 10 + int(s[i])
+            i += 1
+
+        result = sign * num
+
+        if result < -2**31:
+            return -2**31
+
+        if result > 2**31 - 1:
+            return 2**31 - 1
+
+        return result
